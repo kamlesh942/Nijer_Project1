aed00d490b5d427e48abcc8b46d20a3b306b8f24
kamlesh rajak <145656253+kamlesh942@users.noreply.github.com>
2024-11-10 17:14:40 +0530

    footer.js

--- src/component/Footer/footer.js
+++ src/component/Footer/footer.js
@@ -1,31 +1,54 @@
 import React from 'react';
-import './footerStyle.css';
+import './contactStyle.css';
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt, FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
 
- 
-import { FaFacebookF, FaInstagram, FaYoutube } from 'react-icons/fa';
+import Header from '../Nav/nav';
+import Footer from '../Footer/footer';
 
-const Footer = () => {
+const ContactPage = () => {
   return (
-    
+    <div>
+      <Header />
+      <div className="contact-page">
+      
+        <div className="contact-container">
+          <div className="contact-info">
+            <h2>Contact Us</h2>
+            <p>We are here to help you</p>
+            <div className="contact-details">
+              <div className="contact-item">
+                <FaPhoneAlt className="icon" /> 
+                <span>Phone: +91 6263733640</span>
+              </div>
+              <div className="contact-item">
+                <FaEnvelope className="icon" /> 
+                <span>Email: ShivamSahu6263@gmail.com</span>
+              </div>
+              <div className="contact-item">
+                <FaMapMarkerAlt className="icon" /> 
+                <span>Location: Rajendra Nagar, Gali No. 5, Satna (M.P.)</span>
+              </div>
+            </div>
+            
+          </div>
 
-    
-    <footer className="footer">
-
-      <div className="social-icons">
-        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="icon fb-icon">
-          <FaFacebookF />
-        </a>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="icon insta-icon">
-          <FaInstagram />
-        </a>
-        <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className="icon yt-icon">
-          <FaYoutube />
-        </a>
+          <div className="contact-form">
+            <h2>Let's Talk</h2>
+            <p>Feel free to drop us a line below</p>
+            <form>
+              <input type="text" placeholder="Your Name" className="Input" required />
+              <input type="email" placeholder="Email" required />
+              <input type="tel" placeholder="Phone No." required />
+              <textarea placeholder="Message" required></textarea>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
+      
       </div>
-    </footer>
-
-    
+    <Footer />
+    </div>
   );
 };
 
-export default Footer;
+export default ContactPage;
